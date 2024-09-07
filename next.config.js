f505c8f2bc840c5f402cea21cb32836261260a7b
viveksinra <vivek.kr.vks@gmail.com>
2024-09-08 01:22:47 +0530

    updated component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], // Add Cloudinary's domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
   },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
