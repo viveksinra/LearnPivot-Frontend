93b1f1eee81ab31e12f94f695ea1b9d2b1a02dfa
viveksinra <vivek.kr.vks@gmail.com>
2024-09-08 00:24:03 +0530

    updated image issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'], // Add Cloudinary's domain here
+  },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
