710d4cd30afcdc47014b5c5e134c346778fdc44a
viveksinra <vivek.kr.vks@gmail.com>
2025-05-04 11:40:15 +0530

    fix: enhance refund calculation logic in OnePurchasedCourse and OnePurchasedMockTest components, and update PaymentAlert text for clarity

--- app/utils/index.js
+++ app/utils/index.js
@@ -3,7 +3,7 @@
 export const API_ENDPOINT = "https://api.chelmsford11plus.com";
 export const FRONT_ENDPOINT = "https://chelmsford11plus.com";
 
-// export const API_ENDPOINT = "http://localhost:";
+// export const API_ENDPOINT = "http://localhost:2040";
 // export const FRONT_ENDPOINT = "http://localhost:3000";
 
 
